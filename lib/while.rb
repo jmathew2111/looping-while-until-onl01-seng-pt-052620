8d5c05dfebecf34b4f0f5e4f7d323322c9602430
Justin Mathew <jmathew2111@yahoo.com>
2020-06-10 00:56:45 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,12 +1,11 @@
-
-def using_while(levitation_force)  
-  levitation_force = 6
+ def using_while()  
+  #here you HARDCODE the initial value for levitation_force, so why you ever expect to receive it as argument???
+  levitation_force = 6 
    while levitation_force < 10                  
-       levitation_force  += 1                                        
-  puts "Wingardium Leviosa"           
+       levitation_force  += 1                      
+       puts "Wingardium Leviosa"           
   end 
-end                                          
-        
+end
 
 
 
